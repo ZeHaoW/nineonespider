b1f43c16d06a212b5bc20a6282ecd51ea5c70d03
1zehaowang <zehaowang@163.com>
2017-07-22 14:50:42 +0800

    先打印一下url

---
+++ nineoneporn/spiders/91_task.py
@@ -0,0 +1,14 @@
+import scrapy
+from scrapy import Selector
+
+class NineoneSeedsSpider(scrapy.Spider):
+    name = "91_task"
+    start_urls = ["http://email.91dizhi.at.gmail.com.8h3.space/v.php?next=watch"]
+
+    def parse(self, response):
+        selector = Selector(response)
+        content_list = selector.xpath("//div[@class='listchannel']")
+        for content in content_list:
+            content_url = content.xpath("a/@href").extract()
+            title = content.xpath("a/@title").extract()
+            print content_url, title
